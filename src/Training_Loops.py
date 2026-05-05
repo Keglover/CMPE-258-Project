fixed the memory issues, but adversary is not doing gradient ascent properly

src/Training_Loops.py
@@ -79,7 +79,7 @@ class SimpleTrainingLoop():
             )
 
             if train_loss > loss_prev:
-                print(f"Previous loss ({loss_prev}) is less than current epoch loss ({loss}). Stopping early to circumvent overfitting")
+                print(f"Previous loss ({loss_prev}) is less than current epoch loss ({train_loss}). Stopping early to circumvent overfitting")
                 break
 
             if train_loss <= 0.0001:
@@ -88,3 +88,168 @@ class SimpleTrainingLoop():
 
         #test_loss, test_acc = run_eval(model, test_loader)
         #print(f"Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}")
+
+class Adversarial_Loop:
+    def __init__(self, defender, adversary, warm_start_epochs,
+                 train_loader: DataLoader, test_loader: DataLoader,
+                 max_epochs: int = 10, criterion = nn.BCEWithLogitsLoss()):
+        self.defender = defender
+        self.criterion = criterion
+        self.adversary = adversary
+        self.warm_start = warm_start_epochs
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.max_epochs = max_epochs
+        self.defender_token = "def" # Just to help with disambiguation and magic strings
+        self.adversary_token = "adv"
+
+    def _train_defender(self, mode: str):
+        self.defender.to(DEVICE)
+        self.adversary.eval()
+
+        total_loss = 0.0
+        total_acc  = 0.0
+        total_samples = 0
+        total_tp = 0
+        total_tn = 0
+        total_fp = 0
+        total_fn = 0
+
+        loader = None
+
+        if mode.lower() == 'train':
+            loader = self.train_loader
+        elif mode.lower() == 'test':
+            loader = self.test_loader
+        else:
+            raise IndexError(f"No such mode {mode.lower()}")
+
+        for x, y in loader:
+            self.defender.zero_grad()
+
+            batch_perf = None
+            if mode.lower() == 'train':
+                batch_perf = self.defender.batch_eval(x, y, self.criterion)
+            elif mode.lower() == 'test':
+                batch_perf = self.defender.pred(x, y, self.criterion)
+            else:
+                raise IndexError("How did you get here?")
+
+            batch_size = batch_perf["total"]
+
+            total_loss += batch_perf["loss"] * batch_size
+            total_acc  += batch_perf["accuracy"] * batch_size
+            total_tp += batch_perf["tp"] 
+            total_tn += batch_perf["tn"] 
+            total_fp += batch_perf["fp"] 
+            total_fn += batch_perf["fn"] 
+            total_samples += batch_size
+
+        avg_loss = total_loss / total_samples
+        avg_acc  = total_acc / total_samples
+        rate_tp  = total_tp / total_samples
+        rate_fp  = total_fp / total_samples
+        rate_tn  = total_tn / total_samples
+        rate_fn  = total_fn / total_samples
+
+        return {
+            "loss": avg_loss, "accuracy": avg_acc, 
+            "tp_rate": rate_tp, "fp_rate": rate_fp, "tn_rate": rate_tn, "fn_rate": rate_fn
+        }
+
+    def _train_adversary(self):
+        self.adversary.to(DEVICE)
+        self.defender.to(DEVICE)
+        self.defender.eval()
+
+        total_loss = 0.0
+        total_success = 0.0
+        total_batches = 0
+        total_mal_probs = 0.0
+        total_reward = 0.0
+
+        for x, y in self.train_loader:
+            y = y.view(-1)  # Figure out whether or not you need this set dynamically
+
+            mal_mask = y == 1
+            if mal_mask.sum() == 0: # I probably need to change this!
+                continue
+
+            x_mal = x[mal_mask]
+
+            batch_perf = self.adversary.batch_eval(x_mal=x_mal, edit_mask=None, defender=self.defender)
+
+            total_loss      += batch_perf["loss"]
+            total_success   += batch_perf["esr"]
+            total_mal_probs += batch_perf["mean_defender_malware_prob"]
+            total_reward    += batch_perf["reward"]
+            total_batches += 1
+
+        return {
+            "loss":             total_loss      / max(total_batches, 1),
+            "esr":              total_success   / max(total_batches, 1),
+            "avg_malware_prob": total_mal_probs / max(total_batches, 1),
+            "avg_reward":       total_reward    / max(total_batches, 1)
+        }
+
+    def _train_one_epoch(self, model: str, mode: str = "train"):  
+        if model.lower() == self.defender_token:
+            return self._train_defender(mode=mode)
+
+        elif model.lower() == self.adversary_token:
+            return self._train_adversary()
+
+        else:   # Shouldn't get here, but sometimes weird shit happens
+            raise IndexError(f"Adversarial_Loop._train_one_epoch got an unexpected token: {model.lower}")
+
+    def train(self):
+        
+        for epoch in range(self.max_epochs):
+            def_train_performance = self._train_one_epoch(model=self.defender_token, mode='train')
+            def_test_performance  = self._train_one_epoch(model=self.defender_token, mode='test')
+            adv_train_performance = None
+            
+            if epoch >= self.warm_start:
+                adv_train_performance = self._train_one_epoch(self.adversary_token)
+
+            # Defender metrics
+            def_train_loss = def_train_performance["loss"]
+            def_train_acc  = def_train_performance["accuracy"]
+            def_train_tp_rate = def_train_performance["tp_rate"]
+            def_train_tn_rate = def_train_performance["tn_rate"]
+            def_train_fp_rate = def_train_performance["fp_rate"]
+            def_train_fn_rate = def_train_performance["fn_rate"]
+
+            def_test_loss = def_test_performance["loss"]
+            def_test_acc  = def_test_performance["accuracy"]
+            def_test_tp_rate = def_test_performance["tp_rate"]
+            def_test_tn_rate = def_test_performance["tn_rate"]
+            def_test_fp_rate = def_test_performance["fp_rate"]
+            def_test_fn_rate = def_test_performance["fn_rate"]
+
+            print(
+                f"Epoch {epoch+1}/{self.max_epochs} | "
+                f"\n"
+                f"==== DEFENDER METRICS ===="
+                f"\n"
+                f"Train Loss: {def_train_loss:.4f}, Train Acc: {def_train_acc:.4f} | Train TP Rate: {def_train_tp_rate:.4f}, Train FP Rate: {def_train_fp_rate:.4f}, Train TN Rate: {def_train_tn_rate:.4f}, Train FN Rate: {def_train_fn_rate:.4f} |"
+                f"\n"
+                f"Test Loss: {def_test_loss:.4f}, Test Acc: {def_test_acc:.4f} | Test TP Rate: {def_test_tp_rate:.4f}, Test FP Rate: {def_test_fp_rate:.4f}, Test TN Rate: {def_test_tn_rate:.4f}, Test FN Rate: {def_test_fn_rate:.4f} |"
+                f"\n"
+            )
+
+            if adv_train_performance is not None:
+
+                adv_loss       = adv_train_performance["loss"]
+                adv_esr        = adv_train_performance['esr']
+                adv_avg_prob   = adv_train_performance["avg_malware_prob"]
+                adv_avg_reward = adv_train_performance["avg_reward"]
+
+                print(
+                    f"==== ADVERSARY METRICS ===="
+                    f"\n"
+                    f"Loss: {adv_loss:.4f}, ASR: {adv_esr:.4f} |"
+                    f"\n"
+                    f"Average Malware Probability: {adv_avg_prob:.4f}, Average Reward: {adv_avg_reward:.4f} |"
+                    f"\n"
+                )

src/agents.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
 
+from cuda_snapshot import cuda_snapshot
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MAX_LEN = int(2.048 * 10**6)    # 2 MB max input or output size
 
@@ -157,7 +158,9 @@ class Defender(nn.Module):
 
         self.optim.zero_grad()
 
+        cuda_snapshot("DEF BATCH_EVAL: BEFORE")
         logits = self.forward(x)
+        cuda_snapshot("DEF BATCH_EVAL: AFTER")
         loss = loss_fn(logits, labels)
 
         loss.backward()
@@ -200,7 +203,9 @@ class Defender(nn.Module):
             if labels.device is not DEVICE:
                 labels = labels.to(DEVICE)
 
+            cuda_snapshot("DEF PRED: BEFORE")
             logits = self.forward(x)
+            cuda_snapshot("DEF PRED: AFTER")
             loss = loss_fn(logits, labels)
             preds = (torch.sigmoid(logits) >= 0.5).float()
 
@@ -240,7 +245,7 @@ class CNNAttacker(nn.Module):
         self.optim = self._bulid_optim(optim_name)
 
     @torch.no_grad()
-    def _apply_adv_bytes(self, x_mal, adv_bytes) -> torch.Tensor:
+    def _apply_adv_bytes(self, x_mal, adv_bytes, edit_mask=None) -> torch.Tensor:
         """
         x_mal:      (B, T)
         adv_logits: (B, L_adv, 256)
@@ -249,7 +254,9 @@ class CNNAttacker(nn.Module):
             x_adv: (B, T + L_adv) or padded/truncated to defender input length
         """
         
+        cuda_snapshot("APPLY ADV BYTES: Before sampling")
         x_adv = torch.cat([x_mal, adv_bytes], dim=1)    # (B, L_adv)
+        cuda_snapshot("APPLY ADV BYTES: After sampling")
 
         # Optional: truncate/pad if defender expects fixed length
         if x_adv.size(1) > MAX_LEN:
@@ -280,22 +287,50 @@ class CNNAttacker(nn.Module):
     def forward(self, x_bytes, editable_mask=None) -> torch.Tensor:
         x_bytes = x_bytes.to(DEVICE).long()
 
-        x_emb = self.byte_emb(x_bytes)  # (B, T, emb_dim)
+        ATTK_VIEW_LEN = 4096
+        if x_bytes.size(1) > ATTK_VIEW_LEN:
+            head = x_bytes[:, :ATTK_VIEW_LEN // 2]
+            tail = x_bytes[:, -ATTK_VIEW_LEN // 2 :]
+            x_bytes_view = torch.cat([head, tail], dim=1)
+            
+            if editable_mask is not None:
+                head_mask = editable_mask[:, :ATTK_VIEW_LEN // 2]
+                tail_mask = editable_mask[:, -ATTK_VIEW_LEN // 2 :]
+                editable_mask = torch.cat([head_mask, tail_mask], dim=1)
+        else:
+            x_bytes_view = x_bytes
 
         if editable_mask is None:
-            editable_mask = torch.zeros(x_bytes.shape, 
-                                             device=x_bytes.device,
-                                             dtype=torch.float32)
+            editable_mask = torch.zeros(
+                x_bytes_view.shape[0],
+                x_bytes_view.shape[1],
+                device=x_bytes.device,
+                dtype=torch.float32,
+            )
+        else:
+            if editable_mask.shape[1] != x_bytes_view.shape[1]:
+                raise ValueError(
+                    f"editable_mask length {editable_mask.shape[1]} does not match "
+                    f"input view length {x_bytes_view.shape[1]}"
+                )
+            
+            if editable_mask.device != x_bytes.device:
+                editable_mask = editable_mask.to(x_bytes.device)
+            if editable_mask.dtype != torch.float32:
+                editable_mask = editable_mask.float()
 
+        x_emb = self.byte_emb(x_bytes_view)  # (B, T, emb_dim)
         mask_feat = editable_mask.unsqueeze(-1)     # (B, T, 1)
 
+        cuda_snapshot("ADV FORWARD: Before concat")
+        print(f"x_emb.shape: {x_emb.shape}, mask_feat.shape: {mask_feat.shape}")
         x = torch.cat([x_emb, mask_feat], dim=-1)   # (B, T, emb_dim+1)
         x = x.transpose(1, 2)                       # (B, emb_dim+1, T)
-
+        cuda_snapshot("ADV FORWARD: After concat")
         h = self.conv(x).squeeze(-1)                # (B, 128)
         logits = self.decoder(h)                    # (B, adv_len * 256)
         logits = logits.view(
-            x_bytes.size(0), 
+            x_bytes_view.size(0), 
             self.adv_len, 
             self.output_size
         )
@@ -319,7 +354,9 @@ class CNNAttacker(nn.Module):
 
         self.optim.zero_grad()
         
+        cuda_snapshot("ADV BATCH_EVAL: Before forward")
         adv_logits = self.forward(x_mal, edit_mask)
+        cuda_snapshot("ADV BATCH_EVAL: After forward")
         dist = torch.distributions.Categorical(logits=adv_logits)
 
         adv_bytes = dist.sample()